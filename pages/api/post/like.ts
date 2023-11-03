2ffb9bec2c1cd7ef41ce23e109b5c939da5c662f
jc <wnwlcks123@gmail.com>
2023-11-03 14:51:26 +0900

    collection.find() 를 사용하여 좋아요 기능 개선

--- pages/api/post/like.ts
+++ pages/api/post/like.ts
@@ -38,8 +38,8 @@ export default async function handler(요청:요청, 응답:NextApiResponse){
       }
     }
 
-    let userList = await db.collection('like').find().toArray();
-    userList = userList.filter((x:mapItem) => x.post_id == temp.post_id);
+    let userList = await db.collection('like').find({ post_id : temp.post_id}).toArray();
+    // userList = userList.filter((x:mapItem) => x.post_id == temp.post_id);
     return await 응답.status(200).json(userList);
   }
 
